756652900667bf02c5049611ca66c35570a52f45
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2021-09-19 20:17:36 +0630

    19.9.2021

---
+++ jsprojectlthree/feedback/js/app.js
@@ -0,0 +1,9 @@
+// UI
+const panel = document.getElementById('panel');
+const ratingcontainer = document.querySelector('.ratings-container');
+const ratings = document.querySelectorAll('.rating');
+const sendbtn = document.querySelector('.btn');
+
+panel.addEventListener('click',(e)=>{
+    
+});

d891b02e5193b24218e249a9b3cc333e0f99c2b2
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2021-09-27 10:24:15 +0630

    27.9.2021

---
+++ jsprojectslthree/dragndrop/js/app.js
@@ -0,0 +1,61 @@
+// UI
+const picdiv = document.querySelector('.fill'),
+    empties = document.querySelectorAll('.empty');
+
+picdiv.addEventListener('dragstart',dragstart);
+
+picdiv.addEventListener('dragend',dragend);
+
+// Drag Start
+function dragstart(){
+    this.className += " hold";
+    setTimeout(()=>{
+        this.className = "invisible";
+    },0);
+};
+
+// empties.forEach(empty => {
+//     empty.addEventListener('dragover',dragover);
+//     empty.addEventListener('dragenter',dragenter);
+//     empty.addEventListener('dragleave',dragleave);
+//     empty.addEventListener('drop',dragdrop);
+// });
+
+// ES6 can looping  objects-specific iterator value
+for(const empty of empties){
+    console.log(empty);
+    empty.addEventListener('dragover',dragover);
+    empty.addEventListener('dragenter',dragenter);
+    empty.addEventListener('dragleave',dragleave);
+    empty.addEventListener('drop',dragdrop);
+};
+
+// numberable property name of an object
+// for(const empty in empties){
+//      console.log(empty)   
+// }
+
+// Drat End
+function dragend(){
+    this.className = "fill";
+}
+
+function dragover(e){
+    e.preventDefault();
+}
+
+function dragenter(e){
+    e.preventDefault();
+    this.className += " hovered";
+}
+
+function dragleave(e){
+    e.preventDefault();
+    this.className = "empty";
+}
+
+function dragdrop(e){
+    // e.preventDefault();
+    this.className = "empty";
+    this.append(picdiv);
+}
--- jsprojectslthree/feedback/js/app.js
+++ jsprojectslthree/feedback/js/app.js
@@ -4,6 +4,32 @@ const ratingcontainer = document.querySelector('.ratings-container');
 const ratings = document.querySelectorAll('.rating');
 const sendbtn = document.querySelector('.btn');
 
-panel.addEventListener('click',(e)=>{
-    
+let selectedrating = 'Satisfied';
+
+ratingcontainer.addEventListener('click',(e)=>{
+    if(e.target.parentNode.classList.contains("rating")){
+        removeactive();
+        e.target.parentNode.classList.add("active");
+        // selectedrating = e.target.nextElementSibling.textContent;
+        selectedrating = e.target.parentNode.lastElementChild.textContent;
+    }else if(e.target.classList.contains('rating')){
+        removeactive();
+        e.target.classList.add("active");
+        selectedrating = e.target.lastElementChild.textContent;
+    }
+});
+
+function removeactive(){
+    for(let i = 0; i < ratings.length; i++){
+        ratings[i].classList.remove("active");
+    }
+};
+
+sendbtn.addEventListener('click',()=>{
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You</strong>
+        <strong>Feedback : ${selectedrating}</strong>
+        <p>We'll use your feedback to improve our customer support</p> 
+    `;
 });

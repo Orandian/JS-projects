ef815bac732e710b7cdd4b9b5ed9347564bbfe21
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2021-10-27 21:47:08 +0630

    27.10.2021

--- jsprojects/jsprogressbar/app.js
+++ jsprojects/jsprogressbar/app.js
@@ -1,15 +1,15 @@
 const progressBar = document.querySelector('#progress-bar');
 const section = document.querySelector('section');
 
-const animateProgressBar = () => {
-    let scrollDistance = -section.getBoundingClientRect().top;
-    let progressWidth = (scrollDistance / (section.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100;
+const animatedBar = () => {
+    let scrollBar = -section.getBoundingClientRect().top;
+    let progressWidth = (scrollBar / (section.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100 ;
     let value = Math.floor(progressWidth);
-    progressBar.style.width = value + '%';
+    progressBar.style.width = `${value}%`;
 
-    if(value < 0){
+    if(value < 0) {
         progressBar.style.width = '0%';
     }
 }
 
-window.addEventListener('scroll',animateProgressBar);
+window.addEventListener('scroll',animatedBar);
